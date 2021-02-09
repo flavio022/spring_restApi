460dacf956fff3a89073357faad722a2b0a10147
Flavio <flavio.danilo022@gmail.com>
2021-02-09 07:45:44 -0300

    Autorizando requisições para usuarios especificos

--- src/main/java/com/cursomc/config/SecurityConfig.java
+++ src/main/java/com/cursomc/config/SecurityConfig.java
@@ -46,7 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/categorias/**",
             "/estados/**"
     };
-
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/clientes/**",
+            "/auth/forgot/**"
+    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -56,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
--- src/main/java/com/cursomc/resources/ClienteResource.java
+++ src/main/java/com/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +53,7 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
 		Cliente obj = service.fromDTO(objDto);
@@ -60,6 +62,7 @@ public class ClienteResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Cliente obj, @PathVariable Integer id) {
 		obj.setId(id);
